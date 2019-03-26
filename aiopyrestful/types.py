df335090ea4252c53817e5681011b42aadb5e6f4
herui <harryx520@qq.com>
2019-03-26 16:11:14 +0800

    修复参数类型解析的问题

--- aiopyrestful/types.py
+++ aiopyrestful/types.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 
 
-def convert(value, type_):
-    """ Convert / Cast function """
-    if type(value) is str:
-        value = value.encode('utf-8')
-
+def _convert_from_bytes(value, type_):
     if issubclass(type_, bytes):
         return value
     else:
-        data = value.decode('utf-8')
         if issubclass(type_, bool):
-            return False if data.upper() == 'FALSE' else True
+            return False if value.upper() == b'FALSE' else True
+        elif issubclass(type_, str):
+            return value.decode('utf-8')
         else:
-            return type_(data)
+            return type_(value)
+
+
+def convert(value, type_):
+    """ Convert / Cast function """
+    if type(value) is str:
+        if issubclass(type_, str):
+            return value
+        value_bytes = value.encode('utf-8')
+        return _convert_from_bytes(value_bytes, type_)
+    elif type(value) is bytes:
+        return _convert_from_bytes(value, type_)
+    else:
+        return type_(value)
